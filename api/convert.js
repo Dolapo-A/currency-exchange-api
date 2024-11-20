98d5a1ba65459a7c42467c641b3d2ab7aae413a9
Dolapo-crypto <araoyeemmanuel2001@gmail.com>
2024-11-20 15:57:09 +0000

    Add CORS header

--- api/convert.js
+++ api/convert.js
@@ -26,6 +26,20 @@ async function getExchangeRates() {
 }
 
 export default async function handler(req, res) {
+	// Set CORS headers
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	);
+
+	// Handle OPTIONS request for CORS preflight
+	if (req.method === 'OPTIONS') {
+		return res.status(200).end();
+	}
+
 	const rates = await getExchangeRates();
 
 	if (req.url === "/api/rates") {
